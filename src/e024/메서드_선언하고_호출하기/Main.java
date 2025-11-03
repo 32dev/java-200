5f2d2b2f038a294c1c69ca47a125bc895c21f2a8
louis <louis@DESKTOP-7F6CBQH>
2025-11-03 17:04:27 +0900

    ~e025

---
+++ src/e024/메서드_선언하고_호출하기/Main.java
@@ -0,0 +1,12 @@
+package e024.메서드_선언하고_호출하기;
+
+public class Main {
+	public static void show() {
+		System.out.println("*******************************");
+		System.out.println("  이 프로그래밍은 Java200 예제 e014  ");
+		System.out.println("*******************************");
+	}
+	public static void main(String[] args) {
+		show();
+	}
+}
---
+++ src/e025/키보드로_입력받아_콘솔에_출력하기/Main.java
@@ -0,0 +1,20 @@
+package e025.키보드로_입력받아_콘솔에_출력하기;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		System.out.println("정수를 입력하세요.");
+		int choice = scann.nextInt();
+		System.out.println(choice);
+		
+		System.out.println("실수를 입력하세요.");
+		double rchoice = scann.nextDouble();
+		System.out.println(rchoice);
+		
+		System.out.println("문자열을 입력하세요.");
+		String schoice = scann.next();
+		System.out.println(schoice);
+	}
+}
